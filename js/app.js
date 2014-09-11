ef8418358b52c696e91884296123109dc60949d9
Acaspita <acaspita@gmail.com>
2014-09-11 20:46:38 -0300

    portadas

--- js/app.js
+++ js/app.js
@@ -5,10 +5,61 @@ app.controller('MainController', function($scope, $http) {
     $http.get('https://graph.facebook.com/442382955902604?fields=albums.limit(99999999){photos,name,description}')
     .success(function(data) {
         $scope.albums = [];
+        $scope.portadas = [];
         angular.forEach(data.albums.data, function(album, key) {
             if (album.name != "Cover Photos" && album.name != "Profile Pictures") {
                 $scope.albums.push(album);
-            };
+                switch (album.name) {
+                    case "Abeja Silvestre":
+                        $scope.portadas['Abeja Silvestre'] = album.photos.data[0].images[album.photos.data[0].images.length - 1].source;
+                    break;
+                    case "Enjambre":
+                        $scope.portadas['Enjambre'] = album.photos.data[0].images[album.photos.data[0].images.length - 1].source;
+                    break;
+                    case "Colmena Silvestre":
+                        $scope.portadas['Colmena Silvestre'] = album.photos.data[0].images[album.photos.data[0].images.length - 1].source;
+                    break;
+                    case "Colmena Productora":
+                        $scope.portadas['Colmena Productora'] = album.photos.data[0].images[album.photos.data[0].images.length - 1].source;
+                    break;
+                    case "Como SI Actuar":
+                        $scope.portadas['Como SI Actuar'] = album.photos.data[0].images[album.photos.data[0].images.length - 1].source;
+                    break;
+                    case "Como NO Actuar":
+                        $scope.portadas['Como NO Actuar'] = album.photos.data[0].images[album.photos.data[0].images.length - 1].source;
+                    break;
+                    case "Maltratos":
+                        $scope.portadas['Maltratos'] = album.photos.data[0].images[album.photos.data[0].images.length - 1].source;
+                    break;
+                    case "Insectos Relacionados":
+                        $scope.portadas['Insectos Relacionados'] = album.photos.data[0].images[album.photos.data[0].images.length - 1].source;
+                    break;
+                    case "Que producen":
+                        $scope.portadas['Que producen'] = album.photos.data[0].images[album.photos.data[0].images.length - 1].source;
+                    break;
+                    case "":
+                        $scope.portadas[''] = album.photos.data[0].images[album.photos.data[0].images.length - 1].source;
+                    break;
+                    case "Preguntas Frecuentes":
+                        $scope.portadas['Preguntas Frecuentes'] = album.photos.data[0].images[album.photos.data[0].images.length - 1].source;
+                    break;
+                    case "Que producen":
+                        $scope.portadas['Que producen'] = album.photos.data[0].images[album.photos.data[0].images.length - 1].source;
+                    break;
+                    case "Habitantes de la Colmena":
+                        $scope.portadas['Habitantes de la Colmena'] = album.photos.data[0].images[album.photos.data[0].images.length - 1].source;
+                    break;
+                    case "Lo que no vemos de una abeja":
+                        $scope.portadas['Lo que no vemos de una abeja'] = album.photos.data[0].images[album.photos.data[0].images.length - 1].source;
+                    break;
+                    case "Actividades de los ultimos años":
+                        $scope.portadas['Actividades de los ultimos años'] = album.photos.data[0].images[album.photos.data[0].images.length - 1].source;
+                    break;
+                    case "Videos recomendables para conocer sobre la abeja":
+                        $scope.portadas['Videos recomendables para conocer sobre la abeja'] = album.photos.data[0].images[album.photos.data[0].images.length - 1].source;
+                    break;
+                }
+            }
         });
     });
 
